c103b1671c9b384203c0e7578cbd1df7ee9acb43
azell-cells <azrealpurba@gmail.com>
2026-06-09 09:18:11 +0000

    Menambahkan data dummy inventori

--- InventoriBarang/inventori.go
+++ InventoriBarang/inventori.go
@@ -29,5 +29,45 @@ var nTransaksi int = 0
 
 
 func main() {
-	fmt.Println("Aplikasi Inventori")
+	isiDataDummy()
+	fmt.Println("Data dummy berhasil dimuat")
+}
+
+// ==========================================
+// SUBPROGRAM DATA DUMMY
+// ==========================================
+func isiDataDummy() {
+	// Spesifikasi: Mengisi data awal ke dalam array untuk mempermudah pengujian 
+	if nBarang < NMAX {
+		dataBarang[nBarang].ID = 105
+		dataBarang[nBarang].Nama = "Beras"
+		dataBarang[nBarang].Kategori = "Pokok"
+		dataBarang[nBarang].Stok = 50
+		dataBarang[nBarang].Harga = 15000
+		nBarang = nBarang + 1
+	}
+	if nBarang < NMAX {
+		dataBarang[nBarang].ID = 101
+		dataBarang[nBarang].Nama = "Minyak"
+		dataBarang[nBarang].Kategori = "Pokok"
+		dataBarang[nBarang].Stok = 20
+		dataBarang[nBarang].Harga = 18000
+		nBarang = nBarang + 1
+	}
+	if nBarang < NMAX {
+		dataBarang[nBarang].ID = 103
+		dataBarang[nBarang].Nama = "Gula"
+		dataBarang[nBarang].Kategori = "Pokok"
+		dataBarang[nBarang].Stok = 35
+		dataBarang[nBarang].Harga = 14000
+		nBarang = nBarang + 1
+	}
+	if nBarang < NMAX {
+		dataBarang[nBarang].ID = 102
+		dataBarang[nBarang].Nama = "Garam"
+		dataBarang[nBarang].Kategori = "Bumbu"
+		dataBarang[nBarang].Stok = 100
+		dataBarang[nBarang].Harga = 2000
+		nBarang = nBarang + 1
+	}
 }
